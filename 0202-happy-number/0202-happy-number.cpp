332d71399bb2d603c83c88e72ff08dae52f1582b
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-03 23:37:18 +0530

    Time: 2 ms (23.33%), Space: 8.3 MB (38.67%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int getSum(int n)
+    {
+        int sum=0;
+        while(n>0){
+        int digit=n%10;
+         n=n/10;
+        sum+=digit*digit;
+        }
+        return sum;
+
+    }
+    bool isHappy(int n) {
+        
+        unordered_set<int>seen;
+        while(n!=1){
+        if(seen.find(n)!=seen.end())
+        {
+           return false;
+        }
+        else{
+
+            seen.insert(n);
+            n=getSum(n);
+            
+        }
+        
+        }
+        return true;;
+    }
+};
