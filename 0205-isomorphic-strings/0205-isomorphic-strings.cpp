1971fa6f8ca49983d5401b8c712c863b73c39ea3
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-04 16:56:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (7.37%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,30 +5,28 @@ public:
         unordered_set<char>seen;
         if(s.length()!=t.length())
         {
-           return false;
+            return false;
         }
-        
-        for(int i=0;i<s.length();i++)
-        {
+        for(int i=0;i<s.length();i++){
             if(m.find(s[i])!=m.end())
             {
-               if(m[s[i]]!=t[i]) 
-               {
-                return false;
-               }
-               
+                if(m[s[i]]!=t[i])
+                {
+                  return false;
+                }
             }
-            
             else{
-            if(seen.find(t[i])!=seen.end())
-            {
-                return false;
-            }
-                m[s[i]] = t[i];
+               
+                m[s[i]]=t[i];
+                
+                if(seen.find(t[i])!=seen.end())
+                {
+                   return false;;
+                }
                 seen.insert(t[i]);
             }
+
         }
-        
-     return true;
+        return true;
     }
 };
