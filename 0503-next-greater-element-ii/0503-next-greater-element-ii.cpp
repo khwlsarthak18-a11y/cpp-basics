8c4ce63cc56e5d039015919c84d78bf3ab10442c
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-06 15:54:33 +0530

    Time: 1 ms (83.49%), Space: 28.5 MB (63.43%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>ans(nums.size(),-1);
         int n=nums.size();
+        vector<int>ans(n);
         stack<int>st;
-       
-        for(int i=nums.size()*2-1;i>=0;i--){
+        for(int i=n*2-1;i>=0;i--)
+        {
             while(!st.empty() && nums[i%n]>=st.top())
             {
                 st.pop();
             }
-            if(st.empty())
+            if(i<n)
             {
-                ans[i%n]=-1;
-            }
-            else{
-                ans[i%n]=st.top();
+                if(st.empty())
+                {
+                    ans[i%n]=-1;
+                }
+                else{
+                    ans[i%n]=st.top();
+                }
             }
             st.push(nums[i%n]);
-
         }
-       return ans;
-    
+        return ans;
     }
 };
