44334a742ec8dc6343355560220e9fb2db4a0688
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-09 17:05:21 +0530

    Time: 11 ms (65.92%), Space: 35 MB (88.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-   bool CanTake(vector<int>& weights,int mid,int days)
+    bool CanTake(vector<int>&weights,int mid,int days)
     {
         int day=1;
-         int current=0;
+        int current=0;
         for(int i=0;i<weights.size();i++)
         {
-            
-            if(current+weights[i]<=mid)
+            if(current + weights[i]<=mid)
             {
-               current+=weights[i];
+                current+=weights[i];
             }
             else{
                 day++;
                 current=weights[i];
             }
-            
         }
         if(day<=days)
-        return true;
-        return false;
-
-       
+        {
+            return true;
+        }
+        
+     return false;
+        
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int st=*max_element(begin(weights),end(weights));
         int end=accumulate(weights.begin(),weights.end(),0);
-        while(st<end)
-        {
+        while(st<end){
             int mid=st+(end-st)/2;
             if(CanTake(weights,mid,days))
             {
@@ -37,6 +36,7 @@ public:
                 st=mid+1;
             }
         }
-        return end;
+        return st;
+
     }
 };
