7ff8ebd4b6c5cf701f722fd11d3bde6361b97799
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-11 19:47:02 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.6%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string>ans;
+    void ps(int idx,string &digits,string &temp,unordered_map<char,string>&mp)
+    {
+        if(idx>=digits.length())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        char ch=digits[idx];
+        string str=mp[ch];
+        for(int i=0;i<str.length();i++)
+        {
+            temp.push_back(str[i]);
+            ps(idx+1,digits,temp,mp);
+            temp.pop_back();
+        }
+
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.length()==0)
+        {
+            return {};
+        }
+        string temp="";
+        
+        unordered_map<char,string>mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz"; 
+        ps(0,digits,temp,mp);
+        return ans;
+
+    }
+};
