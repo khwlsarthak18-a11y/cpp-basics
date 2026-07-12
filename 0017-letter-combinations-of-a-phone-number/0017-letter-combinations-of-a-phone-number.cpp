ba8e1bee3ed16e305f7e55535bbb7595847718e1
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-12 23:26:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (13.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<string>ans;
-    void ps(int idx,string &digits,string &temp,unordered_map<char,string>&mp)
+    void ps(int idx,string &digits,string &temp, unordered_map<char,string>&mp){
+    if(idx>=digits.length())
     {
-        if(idx>=digits.length())
-        {
-            ans.push_back(temp);
-            return;
-        }
-        char ch=digits[idx];
-        string str=mp[ch];
-        for(int i=0;i<str.length();i++)
-        {
-            temp.push_back(str[i]);
-            ps(idx+1,digits,temp,mp);
-            temp.pop_back();
-        }
-
+        ans.push_back(temp);
+        return;
     }
+    char ch=digits[idx];
+    string str=mp[ch];
+    for(int i=0;i<str.length();i++)
+    {
+        temp.push_back(str[i]);
+        ps(idx+1,digits,temp,mp);
+        temp.pop_back();
+    }
+}
     vector<string> letterCombinations(string digits) {
         if(digits.length()==0)
         {
             return {};
         }
         string temp="";
-        
         unordered_map<char,string>mp;
         mp['2']="abc";
         mp['3']="def";
@@ -33,9 +30,8 @@ public:
         mp['6']="mno";
         mp['7']="pqrs";
         mp['8']="tuv";
-        mp['9']="wxyz"; 
+        mp['9']="wxyz";
         ps(0,digits,temp,mp);
         return ans;
-
     }
 };
