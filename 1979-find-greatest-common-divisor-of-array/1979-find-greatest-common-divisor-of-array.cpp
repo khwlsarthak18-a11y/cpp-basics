6c02849889b6a865cfb3bce8e6d8bd12ea6a167b
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-19 00:06:06 +0530

    Time: 0 ms (100%), Space: 16.1 MB (67.89%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -3,7 +3,7 @@ public:
     int findGCD(vector<int>& nums) {
         int largest = *max_element(begin(nums), end(nums));
         int smallest = *min_element(nums.begin(), nums.end());
-        int gcd=0;
+        int gcd=1;
         for (int i = 1; i <= min(largest, smallest); i++) {
             if (largest % i == 0 && smallest % i == 0) {
                 gcd = i;
