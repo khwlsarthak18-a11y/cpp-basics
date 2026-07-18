3b467c0dac0adad10d841235e61323a1ab1bd6f9
khwlsarthak18-a11y <khwlsarthak18@gmail.com>
2026-07-18 21:11:49 +0530

    Time: 0 ms (100%), Space: 21.9 MB (36.58%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -6,15 +6,14 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-  
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root == NULL) {
+        if(root==NULL)
+        {
             return false;
         }
         queue<pair<TreeNode*,int>>q;
@@ -39,11 +38,9 @@ public:
                 {
                     q.push({node->right,value+node->right->val});
                 }
-
+    
             }
         }
         return false;
-
-
     }
 };
